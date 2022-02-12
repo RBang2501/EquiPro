cf0812b7f34f5bc008f80e6b745daa870fb47085
NetradeepakC <netradeepakc@gmail.com>
2022-02-12 09:00:08 +0530

    Firebase Auth works

--- public/app.js
+++ public/app.js
@@ -1,21 +1,31 @@
-import { getAuth, sendSignInLinkToEmail } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getAuth, sendSignInLinkToEmail } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyCiOpgUYVJDSdJYn9hWvkUwN75fBZenheM",
+    authDomain: "sportech-b3840.firebaseapp.com",
+    projectId: "sportech-b3840",
+    storageBucket: "sportech-b3840.appspot.com",
+    ssagingSenderId: "719432324696",
+    appId: "1:719432324696:web:a77c03a73b1fd24e0f1c2d",
+    measurementId: "G-02DKHENZ02"
+};
+const app = initializeApp(firebaseConfig);
 document.addEventListener("DOMContentLoaded",event => {
     const app = firebase.app();
     console.log(app);
 })
-function emailLogin(emailName)
+document.emailLogin=function emailLogin(emailName)
 {
-    console.log("! "+emailName);
-    ActionCodeSettings={
+    console.log("! "+emailName.value);
+    const ActionCodeSettings={
         url: "https://sportech-b3840.web.app/",
         handleCodeInApp: true
     }
     const auth = getAuth();
-    console.log("!");
     sendSignInLinkToEmail(auth, emailName, ActionCodeSettings)
         .then((userCredential) => {
             // Signed in
-            window.localStorage.setItem('emailForSignIn',email);
+            window.localStorage.setItem('emailForSignIn',emailName.value);
             const user = userCredential.user;
             document.write("Hello "+user.displayName);
             console.log("Hello "+user.displayName);
@@ -28,5 +38,4 @@ function emailLogin(emailName)
             console.log(errorMessage);
             // ..
         });
-    console.log("!");
 }

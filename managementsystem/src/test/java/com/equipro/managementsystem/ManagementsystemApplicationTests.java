c9e4f025da3553310dd2593a90938fc80970b50b
Rahul Jain <rahulcom64@gmail.com>
2023-07-20 05:09:52 +0530

    Spring Init

---
+++ managementsystem/src/test/java/com/equipro/managementsystem/ManagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipro.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c168b4ed4c1ebd0e9412a7e9d7d6b6a500846553
Rakshit Bang <80825327+RBang2501@users.noreply.github.com>
2023-07-20 06:19:02 +0530

    initialized EquipmentService and EquipmentServiceImpl

---
+++ managementsystem/src/main/java/com/equipro/managementsystem/service/EquipmentService.java
@@ -0,0 +1,8 @@
+package com.equipro.managementsystem.service;
+
+import com.equipro.managementsystem.model.Equipment;
+
+import java.util.List;
+
+public interface EquipmentService {
+}
--- managementsystem/src/main/java/com/equipro/managementsystem/service/EquipmentServiceImlpl.java
+++ managementsystem/src/main/java/com/equipro/managementsystem/service/EquipmentServiceImlpl.java
@@ -1,5 +1,14 @@
 package com.equipro.managementsystem.service;
 
-public class EquipmentServiceImlpl {
-    
+import com.equipro.managementsystem.model.Equipment;
+import com.equipro.managementsystem.repository.EquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class EquipmentServiceImlpl implements EquipmentService {
+    @Autowired
+    private EquipmentRepository studentRepository;
 }

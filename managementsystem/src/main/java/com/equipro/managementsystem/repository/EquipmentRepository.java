0ec1693fc13f504b3853dfa9f6dece42f2dc0c5b
Rahul Jain <rahulcom64@gmail.com>
2023-07-20 05:42:14 +0530

    Added Equipment entity class and EquipmentRepository

--- managementsystem/src/main/java/com/equipro/managementsystem/repository/EquipmentRepository.java
+++ managementsystem/src/main/java/com/equipro/managementsystem/repository/EquipmentRepository.java
@@ -1,5 +1,8 @@
 package com.equipro.managementsystem.repository;
-
-public interface EquipmentRepository {
+import com.equipro.managementsystem.model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment,Integer> {
     
 }

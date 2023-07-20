a9097fe340991c62a2c0ad096f6d040157391c52
Dragonglass101 <79797011+Dragonglass101@users.noreply.github.com>
2023-07-20 17:19:32 +0530

    Implemented Add, Fetch and Edit functionality

--- managementsysytem-frontend/src/components/Student.js
+++ managementsysytem-frontend/src/components/Student.js
@@ -18,8 +18,73 @@ function refreshPage() {
 }
 
 export default function Student() {
+  const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [students, setStudents] = useState([]);
+  const [currentStudentId, setCurrentStudentId] = useState(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+
+    if (currentStudentId) {
+      // Update an existing student
+      updateStudent(currentStudentId, student);
+    } else {
+      // Add a new student
+      fetch('http://localhost:8080/student/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(student),
+      })
+        .then(() => {
+          console.log('New Student added');
+          fetchAllStudents(); // Fetch all students after adding a new one to refresh the list
+        })
+        .catch((error) => {
+          console.error('Error adding student:', error);
+        });
+    }
+    // refreshPage();
+  };
+
+  const updateStudent = (studentId, updatedStudentData) => {
+    fetch(`http://localhost:8080/student/update/${studentId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedStudentData),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        fetchAllStudents(); // Fetch all students after updating to refresh the list
+        setCurrentStudentId(null); // Reset currentStudentId after updating
+      })
+      .catch((error) => {
+        console.error('Error updating student:', error);
+      });
+    refreshPage();
+  };
+
+  const fetchAllStudents = () => {
+    fetch('http://localhost:8080/student/getAll')
+      .then((res) => res.json())
+      .then((result) => {
+        setStudents(result);
+      })
+      .catch((error) => {
+        console.error('Error fetching students:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAllStudents();
+  }, []);
+
   return( 
     <>
+      
     </>
   );
 }
